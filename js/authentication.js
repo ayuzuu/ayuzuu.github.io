aa83264afc38610c19fc0348af8237d9a73df17e
ayuzuu <ayuzuuuu@gmail.com>
2017-10-02 02:25:34 +0900

    パスつけた

---
+++ js/authentication.js
@@ -0,0 +1,13 @@
+const password = "ayusawa12";
+input = window.prompt('パスワードを入力してください', '')
+
+// 入力内容が tama の場合は example_tama.html にジャンプ
+if (input == password) {
+//   location.href = '/'
+} else if (input != '' && input != null) {
+    window.alert('パスワードが違います')
+    location.href = '/'
+}else {
+  window.alert('キャンセルされました')
+  location.href = '/'
+}
